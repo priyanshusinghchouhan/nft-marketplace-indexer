4d4f254e0a9d98cbbdccf70104bd7f435b5d0572
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-06 12:25:11 +0530

    ABI: imported abi's

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -1,4 +1,19 @@
 import { Contract } from "ethers";
-import { provider } from "./alchemy";
+import { provider } from "./alchemy.js";
 
-import { NFTMarketplaceABI, NFT_CONTRACT_ABI } from "../abis/abi";
+import { NFTMarketplaceABI, NFT_CONTRACT_ABI } from "../abis/abi.js";
+
+export const MARKETPLACE_ADDRESS = process.env.MARKETPLACE_ADDRESS as string;
+export const NFT_ADDRESS = process.env.NFT_ADDRESS as string;
+
+export const marketplaceContract = new Contract(
+    MARKETPLACE_ADDRESS,
+    NFTMarketplaceABI,
+    provider
+)
+
+export const nftContract = new Contract(
+    NFT_ADDRESS,
+    NFT_CONTRACT_ABI,
+    provider
+)
