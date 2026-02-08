894be1827dadcb07f4260ca6125e06a650aa2f2b
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-08 12:28:14 +0530

    Services: queried database to get the nft's owned by an specific user/waller

--- src/services/nft.service.ts
+++ src/services/nft.service.ts
@@ -25,3 +25,46 @@ export async function upsertNFT(data: {
         }
     });
 }
+
+
+export async function getNFTsByOwner(wallet: string) {
+    const owner = wallet.toLowerCase();
+
+    const nfts = await prisma.nFT.findMany({
+        where: {
+            owner,
+        },
+        select: {
+            tokenId: true,
+            contractAddress: true,
+            tokenURI: true
+        }
+    });
+
+    const listings = await prisma.listing.findMany({
+        where: {
+            seller: owner,
+            status: "ACTIVE",
+        },
+        select: {
+            listingId: true,
+            tokenId: true,
+            price: true,
+        }
+    });
+
+    const listingMaps = new Map(
+        listings.map((l) => [l.tokenId, l])
+    );
+
+    return nfts.map((nft) => {
+        const listing = listingMaps.get(nft.tokenId);
+
+        return {
+           ...nft,
+           listed: Boolean(listing),
+           listingId: listing?.listingId,
+           price: listing?.price,
+        }
+    });
+}
