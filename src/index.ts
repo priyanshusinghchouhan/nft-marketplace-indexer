e51f518c63020073fc034d35c6d47e9d2ba0f1af
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-08 11:12:38 +0530

    App: Added routes and PORT to listen to the requests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import "dotenv/config";
 import { startIndexer } from "./indexer/index.js";
+import marketplaceRoutes from "../src/routes/marketplace.js";
+import  express  from "express";
+
+const app = express();
+const PORT = 5173;
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
+
+app.use("/marketplace", marketplaceRoutes);
 
 
 async function main () {
