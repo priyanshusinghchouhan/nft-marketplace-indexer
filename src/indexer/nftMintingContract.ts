994b1196593113c3b6b98327949425494043facb
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-08 10:42:31 +0530

    Indexers: Added activity services to nftMintingContract

--- src/indexer/nftMintingContract.ts
+++ src/indexer/nftMintingContract.ts
@@ -1,4 +1,5 @@
 import { NFT_ADDRESS, nftMintingContract } from "../config/contracts.js";
+import { logActivity } from "../services/activity.service.js";
 import { upsertNFT } from "../services/nft.service.js";
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
@@ -6,27 +7,41 @@ const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 export function startNFTTransferListener() {
   console.log("Listening to ERC721 events...");
 
-  // Transfer 
+  // Transfer
   nftMintingContract.on(
     "Transfer",
     async (from, to, tokenId, event) => {
       const tokenIdStr = tokenId.toString();
-      if(to === ZERO_ADDRESS) return;
+      if (to.toLowerCase() === ZERO_ADDRESS) return;
 
       let tokenURI: string | undefined = undefined;
-      if(from === ZERO_ADDRESS) {
-        try{
+      if (from === ZERO_ADDRESS) {
+        try {
           tokenURI = await nftMintingContract.tokenURI(tokenId);
         } catch {
           tokenURI = undefined;
         }
       }
 
+      if (from === ZERO_ADDRESS) {
+        await logActivity({
+          type: "MINTED",
+          wallet: to,
+          txHash: event.transactionHash,
+        });
+      } else {
+        await logActivity({
+          type: "TRANSFER",
+          wallet: to,
+          txHash: event.transactionHash,
+        });
+      }
+
       await upsertNFT({
         contractAddress: NFT_ADDRESS.toLowerCase(),
         owner: to.toLowerCase(),
         tokenId: tokenIdStr,
-        tokenURI
+        tokenURI,
       });
 
       console.log("Indexed NFT", {
@@ -35,6 +50,6 @@ export function startNFTTransferListener() {
         isMint: from === ZERO_ADDRESS,
         txHash: event.transactionHash,
       });
-    }
+    },
   );
 }
