dbd0002ab6d13e547fa3cad800bdf71221c87896
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-08 11:11:06 +0530

    Routes: added route to get all the listings on marketplace

---
+++ src/routes/marketplace.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { getActiveListing } from "../services/listing.service";
+
+const router = Router();
+
+router.get("/listing", async(req, res) => {
+    try {
+        const listings = await getActiveListing();
+
+        return res.json({
+            count: listings.length,
+            listings
+        })
+    }catch(e) {
+        console.log("Failed to fetch listings", e);
+        return res.status(500).json({ e : "Internal server error" });
+    }
+});
+
+export default router;
