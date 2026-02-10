070467d85be369791c469310b3d05e0753d152fe
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-10 19:32:35 +0530

    feat(route): added route to get the listed NFT's for specific status and user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { getNFTsByOwner } from "../services/nft.service";
+import { ListingStatus } from "@prisma/client";
+import { getUserListings } from "../services/listing.service";
 
 const router = Router();
 
@@ -19,5 +21,39 @@ router.get("/:wallet/nfts", async(req, res) => {
         return res.status(500).json({ e : "Internal server error" });
     }
 });
+
+
+router.get("/:wallet/listings", async(req, res)=> {
+    try{
+
+        const { wallet } = req.params;
+        const { status } = req.query;
+
+        let parsedStatus: ListingStatus| undefined;
+
+        if(status) {
+            const upper = String(status).toUpperCase();
+            if(!Object.values(ListingStatus).includes(upper as ListingStatus)) {
+                return res.status(400).json({ error: "Invalid status filter"});
+            }
+
+            parsedStatus = upper as ListingStatus;
+        }
+
+        const listings = await getUserListings(wallet, parsedStatus);
+
+        console.log(`For status: ${status}, total ${listings.length} listings for ${wallet}`);
+
+        return res.json({
+            wallet: wallet.toLowerCase(),
+            count: listings.length,
+            listings,
+        })
+
+    }catch(e) {
+        console.error("Failed to fetch listings", e);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 export default router;
