e450464c97bb945113245acbe7d4dd8d073844b4
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-10 22:02:46 +0530

    Added activity route to listen

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import { startIndexer } from "./indexer/index.js";
 import marketplaceRoutes from "../src/routes/marketplace.js";
-import userRoutes from "../src/routes/users.js"
+import userRoutes from "../src/routes/users.js";
+import activityRoutes from "../src/routes/activity.js";
 import  express  from "express";
 
 const app = express();
@@ -14,6 +15,7 @@ app.listen(PORT, (err) => {
 
 app.use("/marketplace", marketplaceRoutes);
 app.use("/users", userRoutes);
+app.use("/activity", activityRoutes);
 
 
 async function main () {
