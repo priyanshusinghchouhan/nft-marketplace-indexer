82dd27d84c22c75733d5d7a36f6a90126f2b0cbe
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-12 13:32:19 +0530

    main: added CORS

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ import marketplaceRoutes from "../src/routes/marketplace.js";
 import userRoutes from "../src/routes/users.js";
 import activityRoutes from "../src/routes/activity.js";
 import  express  from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5173;
+
+app.use( cors ({
+    origin: "http://localhost:3000"
+}))
 
 app.listen(PORT, (err) => {
     if (err) console.log(err);
